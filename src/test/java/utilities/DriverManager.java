65558998c40b4736759eef95eaab25e8ab844125
Subrat88 <46335561+MyAutomationLabs@users.noreply.github.com>
2024-11-08 01:55:50 +1030

    Update DriverManager.java

--- src/test/java/utilities/DriverManager.java
+++ src/test/java/utilities/DriverManager.java
@@ -4,10 +4,15 @@ import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
 import org.testng.annotations.BeforeSuite;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class DriverManager {
 
@@ -24,28 +29,29 @@ public class DriverManager {
 
     // Get WebDriver instance for the current thread
     public static WebDriver getDriver() {
-        // if (driver.get() == null) {
-        //     System.out.println("No WebDriver instance found, creating a new one...");
-        //    //WebDriverManager.chromedriver().setup(); //Not required in case of latest selenium jar
 
-        //     // Automatically downloads and sets up ChromeDriver
-        //     driver.set(new ChromeDriver()); // Correctly set the ChromeDriver instance in ThreadLocal
-        //     //driver.get().manage().window().maximize();
-        //     driver.get().manage().window().setSize(new Dimension(1600, 1080));  // Replace 1920x1080 with your resolution
+       /* if (driver.get() == null) {
+            System.out.println("No WebDriver instance found, creating a new one...");
+           //WebDriverManager.chromedriver().setup(); //Not required in case of latest selenium jar
 
-        // }
-          if (driver.get() == null) {
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--headless");
-        options.addArguments("--no-sandbox");
-        options.addArguments("--disable-dev-shm-usage");
+            // Automatically downloads and sets up ChromeDriver
+            driver.set(new ChromeDriver()); // Correctly set the ChromeDriver instance in ThreadLocal
+            //driver.get().manage().window().maximize();
+            driver.get().manage().window().setSize(new Dimension(1600, 1080));  // Replace 1920x1080 with your resolution
 
-        try {
-            driver.set(new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), options));
-        } catch (MalformedURLException e) {
-            e.printStackTrace();
+        }*/
+        if (driver.get() == null) {
+            ChromeOptions options = new ChromeOptions();
+            options.addArguments("--headless");
+            options.addArguments("--no-sandbox");
+            options.addArguments("--disable-dev-shm-usage");
+
+            try {
+                driver.set(new RemoteWebDriver(new URL("http://localhost:4444/wd/hub"), options));
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            }
         }
-    }
         return driver.get();
     }
 
